1f76092cc168c9d68f7c1a5814ce1f26992de0aa
David Leal <david@davidleal.com>
2025-03-15 14:54:16 +0000

    Allow up to 7 exercise options

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,17 +3,21 @@ module ApplicationHelper
     color =
       case priority
       when 1
-        "text-lime-400"
+        "text-lime-300"
       when 2
-        "text-lime-700"
+        "text-lime-600"
       when 3
-        "text-amber-300"
+        "text-amber-200"
       when 4
         "text-orange-300"
       when 5
         "text-amber-600"
+      when 6
+        "text-orange-600"
+      when 7
+        "text-red-500"
       end
 
-    content_tag(:span, %w[~ S A B C D][priority], class: color)
+    content_tag(:span, %w[~ S A B C D E F][priority], class: color)
   end
 end
