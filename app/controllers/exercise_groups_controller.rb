37679c4b1412992595af4e3b30e3635d0af67a38
David Leal <david@davidleal.com>
2025-03-15 14:51:46 +0000

    Add ability to add en exercise group

---
+++ app/controllers/exercise_groups_controller.rb
@@ -0,0 +1,15 @@
+class ExerciseGroupsController < ApplicationController
+  def create
+    exercise_group = ExerciseGroup.new(exercise_group_params)
+
+    if exercise_group.save
+      redirect_back fallback_location: training_sessions_path
+    else
+      raise exercise_group.errors.inspect
+    end
+  end
+
+  private def exercise_group_params
+    params.expect(exercise_group: [ :name ])
+  end
+end
