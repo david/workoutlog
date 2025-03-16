c00454366ca30a5b001dcdf0f064f1b52f92c63c
David Leal <david@davidleal.com>
2025-03-16 22:13:12 +0000

    Stop requiring the existence of an exercise group

--- app/controllers/training_sessions_controller.rb
+++ app/controllers/training_sessions_controller.rb
@@ -6,19 +6,21 @@ class TrainingSessionsController < ApplicationController
   def show
     training_session = current_user.training_sessions.
       find_or_initialize_by(session_on: params[:session_on])
+    exercise_group_id = params[:exercise_group_id]
 
-    if params[:exercise_group_id]
-      render locals: {
-        exercise_choices: training_session.exercise_choices.includes(:exercise_option),
-        exercise_group: current_user.exercise_groups.find(params[:exercise_group_id]),
-        exercise_groups: current_user.exercise_groups.all,
-        training_session:
-      }
-    else
+    if exercise_group_id.blank? && current_user.exercise_groups.exists?
       redirect_to exercise_group_training_session_path(
         training_session,
         current_user.exercise_groups.first
       )
+    else
+      render locals: {
+        exercise_choices: training_session.exercise_choices.includes(:exercise_option),
+        exercise_group: exercise_group_id.presence &&
+          current_user.exercise_groups.find_by(id: exercise_group_id),
+        exercise_groups: current_user.exercise_groups.all,
+        training_session:
+      }
     end
   end
 end
