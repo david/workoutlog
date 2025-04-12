1d1aa658a9a0bb5b0682c91b2a0bf4ecd5a333f5
David Leal <david@davidleal.com>
2025-04-12 13:04:03 +0100

    Move new exercise option form to dedicated page

--- app/controllers/exercise_options_controller.rb
+++ app/controllers/exercise_options_controller.rb
@@ -1,5 +1,12 @@
 class ExerciseOptionsController < ApplicationController
   def new
+    exercise = current_user.exercises.find(params[:exercise_id])
+
+    render locals: {
+      exercise:,
+      exercise_option: exercise.exercise_options.build,
+      training_session: current_training_session
+    }
   end
 
   def create
@@ -7,13 +14,13 @@ class ExerciseOptionsController < ApplicationController
     exercise_option = exercise.exercise_options.build(exercise_option_params)
 
     if exercise_option.save
-      redirect_back fallback_location: training_sessions_path
+      redirect_to training_session_exercise_path(current_training_session, exercise)
     else
       raise exercise_option.errors.inspect
     end
   end
 
   private def exercise_option_params
-    params.expect(exercise_option: %i[description name reps])
+    params.require(:exercise_option).permit(:description, :name, :reps)
   end
 end
---
+++ test/controllers/exercise_options_controller_test.rb
@@ -0,0 +1,36 @@
+require "test_helper"
+
+class ExerciseOptionsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    @exercise = exercises(:one)
+    @training_session = @user.training_sessions.today
+
+    sign_in_as(@user)
+  end
+
+  test "should get new" do
+    get new_exercise_exercise_option_url(@exercise)
+    assert_response :success
+  end
+
+  test "should create exercise option" do
+    assert_difference("ExerciseOption.count") do
+      post exercise_exercise_options_url(@exercise), params: {
+        exercise_option: {
+          name: "New Option",
+          description: "Test description",
+          reps: 10
+        }
+      }
+    end
+
+    assert_redirected_to training_session_exercise_url(@training_session, @exercise)
+
+    new_option = @exercise.exercise_options.last
+
+    assert_equal "New Option", new_option.name
+    assert_equal "Test description", new_option.description
+    assert_equal 10, new_option.reps
+  end
+end
