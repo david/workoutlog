b7c6ab154026f3ca8d78baffb363fae191a92be8
David Leal <david@davidleal.com>
2025-04-12 17:12:08 +0100

    Move new exercise form to dedicated page

--- test/controllers/exercises_controller_test.rb
+++ test/controllers/exercises_controller_test.rb
@@ -37,4 +37,16 @@ class ExercisesControllerTest < ActionDispatch::IntegrationTest
     )
     assert_select "form", 0
   end
+
+  test "should create exercise" do
+    exercise_name = "New Exercise"
+
+    assert_difference("@user.exercises.count") do
+      post exercises_url, params: { exercise: { name: exercise_name } }
+    end
+
+    new_exercise = @user.exercises.find_by!(name: exercise_name)
+
+    assert_redirected_to exercise_url(new_exercise)
+  end
 end
