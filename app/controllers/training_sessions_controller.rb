c239d367b4c26f28b75fdb374c46491e78574ed7
David Leal <david@davidleal.com>
2025-04-12 12:16:32 +0100

    [training sessions] Never redirect to a single exercise

--- app/controllers/training_sessions_controller.rb
+++ app/controllers/training_sessions_controller.rb
@@ -8,10 +8,7 @@ class TrainingSessionsController < ApplicationController
       redirect_to training_session_path(current_user.training_sessions.today)
     elsif current_training_session.today?
       if current_user.exercises.exists?
-        redirect_to training_session_exercise_path(
-          current_training_session,
-          current_user.exercises.first
-        )
+        redirect_to training_session_exercises_path(current_training_session)
       else
         redirect_to new_training_session_exercise_path(current_training_session)
       end
